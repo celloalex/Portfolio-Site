1f1c7e40b71f8b20d35d1fb04c483c85a63bf612
celloalex <alexbuchaniec@gmail.com>
2017-06-27 23:22:22 -0500

    Portfolio
    Portfolio Site

---
+++ Portfolio Site/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Portfolio_Site.Startup))]
+namespace Portfolio_Site
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
